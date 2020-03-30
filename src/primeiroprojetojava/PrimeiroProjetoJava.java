8f484f292e5928985666ecf256d81ec4b39d1c60
Andressa <andressaszx@gmail.com>
2020-03-29 23:09:30 -0300

    finalizando first app

--- src/primeiroprojetojava/PrimeiroProjetoJava.java
+++ src/primeiroprojetojava/PrimeiroProjetoJava.java
@@ -5,6 +5,10 @@
  */
 package primeiroprojetojava;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+import java.util.Date;
+
 /**
  *
  * @author Andressa Silva
@@ -16,7 +20,15 @@ public class PrimeiroProjetoJava {
      */
     public static void main(String[] args) {
         
-        System.out.println("Retornando o projeto inicial");
-        // TODO code application logic here
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        
+        System.out.println(order);
+        
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        
+        System.out.println(os1);
+        System.out.println(os2);
     }
 }
